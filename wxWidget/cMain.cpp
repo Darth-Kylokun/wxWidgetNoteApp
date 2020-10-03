af8fa137e5d8b006984dac78992acb7cb7851d86
Karsten <ftwkrc@gmail.com>
2020-10-02 22:18:07 -0400

    Added in exit and save command

--- wxWidget/cMain.cpp
+++ wxWidget/cMain.cpp
@@ -1,16 +1,48 @@
 #include "cMain.h"
 
 wxBEGIN_EVENT_TABLE(cMain, wxFrame)
-	
+	EVT_MENU(10002, save)
+	EVT_MENU(10003, exit)
 wxEND_EVENT_TABLE()
+
+//fonts[0] is normal
+//fonts[1] is bold
+//fonts[2] is italic
 
 cMain::cMain()
 	: wxFrame{ nullptr, wxID_ANY, "Totally not a Notepad rip off", wxPoint(30, 30), wxSize(800, 600) },
-	richTextCtrl{ new wxRichTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxVSCROLL | wxHSCROLL | wxBORDER_DEFAULT | wxWANTS_CHARS) },
-	fonts{ wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL), wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD), wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_NORMAL) }
+	  richTextCtrl{ new wxRichTextCtrl(this, wxID_ANY, wxEmptyString, wxDefaultPosition, wxDefaultSize, wxVSCROLL | wxHSCROLL | wxBORDER_DEFAULT | wxWANTS_CHARS) },
+	  fonts{ wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_NORMAL), wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_NORMAL, wxFONTWEIGHT_BOLD), wxFont(12, wxFONTFAMILY_ROMAN, wxFONTSTYLE_ITALIC, wxFONTWEIGHT_NORMAL) },
+	  menuBar{ new wxMenuBar() }
 {
-	//fonts[0] is normal
-	//fonts[1] is bold
-	//fonts[2] is italic
 	richTextCtrl->SetFont(fonts[0]);
+
+	wxMenu* menu = new wxMenu();
+	menu->Append(10001, "Open");
+	menu->Append(10002, "Save");
+	menu->Append(10003, "Exit");
+
+	menuBar->Append(menu, "File");
+
+	this->SetMenuBar(menuBar);
+}
+
+void cMain::save(wxCommandEvent& evt)
+{
+	wxFileDialog* saveFileDialog = new wxFileDialog(this, "Save File: ");
+	if (saveFileDialog->ShowModal() == wxID_CANCEL)
+		return;
+
+	wxFileOutputStream stream(saveFileDialog->GetPath());
+	if (!stream.IsOk())
+	{
+		wxLogError("Cannont save file in '%s'", saveFileDialog->GetPath());
+		return;
+	}
+}
+
+void cMain::exit(wxCommandEvent& evt)
+{
+	this->Close();
+	evt.Skip();
 }
--- wxWidget/cMain.h
+++ wxWidget/cMain.h
@@ -1,6 +1,8 @@
 #include <array>
 #include "wx/wx.h"
 #include "wx/richtext/richtextctrl.h"
+#include "wx/filedlg.h"
+#include "wx/wfstream.h"
 #pragma once
 
 class cMain : public wxFrame
@@ -8,9 +10,12 @@ class cMain : public wxFrame
 public:
 	cMain();
 
-	wxRichTextCtrl* richTextCtrl = nullptr;
-
-	std::array<wxFont, 3> fonts;
+	void save(wxCommandEvent& evt);
+	void exit(wxCommandEvent& evt);
 
 	wxDECLARE_EVENT_TABLE();
+private:
+	std::array<wxFont, 3> fonts;
+	wxRichTextCtrl* richTextCtrl = nullptr;
+	wxMenuBar* menuBar = nullptr;
 };
